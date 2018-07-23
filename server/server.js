14592b59346bb644af774149946eb2ab20d25f8b
geokeloglou <geokeloglou@gmail.com>
2018-07-23 18:06:22 +0300

    Add geolocation support via geolocation api

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
-const publicPath = path.join(__dirname, '/../public');
+const {
+    generateMessage,
+    generateLocationMessage
+} = require('./utils/message');
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
 var server = http.createServer(app);
@@ -15,18 +18,18 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
-    socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
 
     socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
-    
-    socket.on('createMessage', (message) => {
+
+    socket.on('createMessage', (message, callback) => {
         console.log('createMessage', message);
-        io.emit('newMessage', generateMessage(message.form, message.text));
-        // socket.broadcast.emit('newMessage', {
-        //     from: message.from,
-        //     text: message.text,
-        //     createdAt: new Date().getTime()
-        // });
+        io.emit('newMessage', generateMessage(message.from, message.text));
+        callback('This is from the server.');
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     socket.on('disconnect', () => {
@@ -35,5 +38,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}`);
+    console.log(`Server is up on ${port}`);
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,9 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {
+    generateMessage,
+    generateLocationMessage
+} = require('./message');
 
 describe('generateMessage', () => {
     it('should generate corrent message object', () => {
@@ -9,6 +12,25 @@ describe('generateMessage', () => {
         var message = generateMessage(from, text);
 
         expect(message.createdAt).toBeA('number');
-        expect(message).toInclude({from, text,});
+        expect(message).toInclude({
+            from,
+            text,
+        });
+    });
+});
+
+describe('generateLocationMessage', () => {
+    it('should generate corrent location object', () => {
+        var from = 'Deb';
+        var latitude = 15;
+        var longitude = 19;
+        var url = 'https://www.google.com/maps?q=15,19';
+        var message = generateLocationMessage(from, latitude, longitude);
+
+        expect(message.createdAt).toBeA('number');
+        expect(message).toInclude({
+            from,
+            url,
+        });
     });
 });
